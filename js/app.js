d096ad8d7c62c22647c3f9766b55a3a10e08fea4
charls96 <charly.lopez.perez@gmail.com>
2022-01-21 23:50:47 +0100

    reading selects values

--- js/app.js
+++ js/app.js
@@ -1,14 +1,57 @@
+const brand = document.querySelector('#marca');
 const result = document.querySelector('#resultado');
 const year = document.querySelector('#year');
+const minimum = document.querySelector('#minimum');
+const maximum = document.querySelector('#maximum');
+const doors = document.querySelector('#puertas');
+const transmision = document.querySelector('#transimision');
+const color = document.querySelector('#color');
 
 const max = new Date().getFullYear()-2;
 const min = max - 10;
 
+const searchData = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     showCars();
     loadYearsSelect();
-})
+});
 
+brand.addEventListener('change', () => {
+    searchData.marca = e.target.value;
+});
+
+year.addEventListener('change', () => {
+    searchData.year = e.target.value;
+});
+
+minimum.addEventListener('change', () => {
+    searchData.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', () => {
+    searchData.maximo = e.target.value;
+});
+
+doors.addEventListener('change', () => {
+    searchData.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', () => {
+    searchData.transmision = e.target.value;
+});
+
+color.addEventListener('change', () => {
+    searchData.color = e.target.value;
+});
 
 
 function showCars(){
